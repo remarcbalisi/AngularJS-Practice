ccefcc87363851bb60c1bdf487ed17cd1dcc35fe
remarcbalisi <remarc.balisi@gmail.com>
2016-06-10 04:05:22 +0800

    custom directives

--- codeschool/controller.js
+++ codeschool/controller.js
@@ -45,14 +45,33 @@
         }
     ];
 
-    app.controller('PanelController', function(){
-        this.tab = 1;
+    //this is move to the directive
+    // app.controller('PanelController', function(){
+    //     this.tab = 1;
+    //
+    //     this.selectTab = function(setTab){
+    //         this.tab = setTab;
+    //     }
+    //     this.isSelected = function(selectedTab){
+    //         return this.tab === selectedTab;
+    //     }
+    // });
 
-        this.selectTab = function(setTab){
-            this.tab = setTab;
-        }
-        this.isSelected = function(selectedTab){
-            return this.tab === selectedTab;
+    app.directive('productPanels', function(){
+        return {
+            restrict:'E',
+            templateUrl: 'product-panels.html',
+            controller:function(){
+                this.tab = 1;
+
+                this.selectTab = function(setTab){
+                    this.tab = setTab;
+                }
+                this.isSelected = function(selectedTab){
+                    return this.tab === selectedTab;
+                }
+            },
+            controllerAs: 'panel'
         }
     });
 
